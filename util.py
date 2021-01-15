72258661f981cb55306e0d0a34f6aa497b629ab0
Distill-pixel <52331239+Distill-pixel@users.noreply.github.com>
2021-01-15 14:19:54 +0530

    Create util.py

---
+++ util.py
@@ -0,0 +1,53 @@
+import datetime
+
+class Colors:
+
+	reset='\033[0m'
+	bold='\033[01m'
+	disable='\033[02m'
+	underline='\033[04m'
+	reverse='\033[07m'
+	strikethrough='\033[09m'
+	invisible='\033[08m'
+	
+	class fg:
+		black='\033[30m'
+		red='\033[31m'
+		green='\033[32m'
+		orange='\033[33m'
+		blue='\033[34m'
+		purple='\033[35m'
+		cyan='\033[36m'
+		lightgrey='\033[37m'
+		darkgrey='\033[90m'
+		lightred='\033[91m'
+		lightgreen='\033[92m'
+		yellow='\033[93m'
+		lightblue='\033[94m'
+		pink='\033[95m'
+		lightcyan='\033[96m'
+		
+	class bg:
+		black='\033[40m'
+		red='\033[41m'
+		green='\033[42m'
+		orange='\033[43m'
+		blue='\033[44m'
+		purple='\033[45m'
+		cyan='\033[46m'
+		lightgrey='\033[47m'
+
+def info(content):
+		print(f"[{Colors.fg.blue}i{Colors.reset}] {content}")
+
+def warning(content):
+		print(f"[{Colors.fg.red}!{Colors.reset}] {content}")
+
+def throw_error(content):
+	with open("errors.txt", "a") as f:
+		f.write(f"[{datetime.datetime.utcnow()}] {content}\n")
+		f.close()
+	return True
+
+def ask(content):
+	return input(f"[{Colors.fg.green}>{Colors.reset}] {content}")
